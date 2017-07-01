37f14cc44e3cb644f4d07707b7f0a37df9c581ed
Ian Wood <ian.wood108@gmail.com>
2017-07-01 16:44:45 -0400

    Added country and state data

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,3 +1,11 @@
+// Source of country and states date: http://www.cssscript.com/generic-country-state-dropdown-list-countries-js/
+
 $(document).ready(function() {
-	
+	populateCountries("country", "prov");
+
+	$("#create-account-form").submit(function(e){
+		e.preventDefault();
+
+		// AJAX
+	});
 });
