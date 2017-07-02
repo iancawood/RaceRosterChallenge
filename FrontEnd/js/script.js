418b1688df2d32673b8c7aa0ba27bf38719e634e
Ian Wood <ian.wood108@gmail.com>
2017-07-02 10:25:23 -0400

    Validate required fields and postal code

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -3,12 +3,48 @@
 $(document).ready(function() {
 	populateCountries("country", "prov");
 
+	var validateRequired = function() {
+		var error = false;
+
+		$(".required-field").each(function() {
+			if ($(this).val().length == 0) {
+				error = true;
+				$(this).parent().addClass("has-error");
+			} else {
+				$(this).parent().removeClass("has-error");
+			}
+		});
+
+		return error;
+	};
+
+	var validatePostal = function() {
+		var error = false;
+
+		if ($("#country :selected").val() === "Canada") {
+			var reg = /[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] ?[0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]/;
+			var postal = $("#postal");
+			
+			if (reg.test(postal.val().toUpperCase())) {				
+				postal.parent().removeClass("has-error");
+			} else {
+				error = true;								
+				postal.parent().addClass("has-error");
+			}
+		}
+
+		return error;
+	};
+
 	$("#create-account-form").submit(function(e){
 		e.preventDefault();
 
-		// validate POSTAL CODE
+		var requiredError = validateRequired();
+		var postalError = validatePostal();
 
-		// AJAX
+		if (!requiredError && !postalError) {
+			console.log("Handle ajax here");
+		}
 	});
 
 	$("#country").change(function() {
